0d97c974f2f2b1bc6d0650c948ba7ca0a53511d4
Piyush Sharma <sharma.076piyush@gmail.com>
2025-01-16 07:54:25 +0530

    initial commit

---
+++ src/main/java/com/mypracticepiyush/rabbitmq/RabbitmqPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.mypracticepiyush.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPracticeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test rabbitmq main");
+		SpringApplication.run(RabbitmqPracticeApplication.class, args);
+	}
+
+}
